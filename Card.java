69a94fc210861d7d373c089fa9e2e569b7fd4e35
Hwanghi <heeda512@naver.com>
2020-08-05 11:02:37 +0900

    시험 삼아 보낸 풀-리퀘스트 입니다.

--- Card.java
+++ Card.java
@@ -5,12 +5,13 @@ public class Card {
 	int getNumber() {return number;}
 	String getShape() {return shape;}
 	
-	boolean isSame(Card c) {
+	boolean isequals(Object o) {
+		Card c = (Card) o;
 		if (c.getNumber()==this.getNumber()) //카드 c의 숫자와 이 카드의 숫자가 동일하면
 			return true;
 		else
 			return false;
-	} //두 카드가 동일한 숫자를 가지고 있는지 확인하는 함수. 게임 총괄 진행 클래스로 옮겨도 될 것 같은데ㅜ 애매해서 여기에 둡니다.
+	} // Object 클래스가 가지고 있는 디폴트 메소드 isequals로 변경
 	
 	//Player가 Card를 상속받아서.. Card클래스에 master 변수는 생성이 안 될 것 같네요..
 }
